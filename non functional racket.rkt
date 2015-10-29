31ba27d2950415f03021dd798ea1ff4c6c759254
John Gallagher <johnjon8@gmail.com>
2015-10-29 12:18:51 -0400

    attempt to make  for loop
    well this isn't working as expected...

--- non functional racket.rkt
+++ non functional racket.rkt
@@ -2,3 +2,11 @@
 
 ;; Lets implement java-esque features in Racket!
 
+;; First, the "for" loop
+
+;; for : Number [Number Number -> Boolean] [Number -> Number] [Number X -> Y]  X  Y-> Y 
+(define (for count stop-case iterate func input output)
+  (if (stop-case count)
+      (func count input)
+      (for (iterate count) stop-case iterate func input (func count input))))
+      
